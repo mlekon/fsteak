f83c31ce2caba231dd3c81b332f83fa50fe93b7f
mlekon <mjlekon@gmail.com>
2014-08-18 22:31:06 -0400

    Completed documenting the code. Added a license. Did stuff to the readme.

--- fsteak/fsteak.cpp
+++ fsteak/fsteak.cpp
@@ -1,3 +1,22 @@
+/*
+ * fsteak: A library for steak-based file I/O
+ * fsteak.c: Definitions of the fsteak library functions
+ * Copyright (C) 2014  Mike Lekon
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "fsteak.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,7 +27,12 @@ struct steak
 	FILE* f;
 };
 
-/* Change the delicious preparation instructions to something a little more usable */
+//////////////////////////////////////////////////////////////////////////
+/// Helper function that converts from the bland, tasteless fopen modes and
+/// the juicy, flavorful fsteak preparation instructions.
+/// prep_instructions: c string with the list of prep instructions to use
+/// returns: The fopen equivalents to the given prep instructions
+//////////////////////////////////////////////////////////////////////////
 char* convert_mode(const char* prep_instructions)
 {
 	int mode_length = strlen(prep_instructions);
@@ -33,12 +57,19 @@ char* convert_mode(const char* prep_instructions)
 	return mode;
 }
 
-/* Place a new steak on the grill (fopen) 
-g(rilled) = r
-s(easoned) = w
-r(are) = b
-c(ontinue) = a
-*/
+
+//////////////////////////////////////////////////////////////////////////
+/// Places a new steak on the grill. 
+/// Roughly equivalent to fopen.
+/// steak_name: A c-string with the name of this steak. Gonna be so good that it needs a proper name 
+/// prep_instructions: A c-string describes how the steak will be prepared. Possible values with
+///		their fopen equivalents are shown below:
+///			g(rilled) = r
+///			s(easoned) = w
+///			r(are) = b
+///			c(ontinue) = a
+/// returns: A steak object used in the other fsteak functions
+//////////////////////////////////////////////////////////////////////////
 steak fsteak(const char* steak_name, const char* prep_instructions)
 {
 	char* mode = convert_mode(prep_instructions);
@@ -50,19 +81,32 @@ steak fsteak(const char* steak_name, const char* prep_instructions)
 	return s;
 }
 
-/* Grill the given steak with a certain amount of BTUs, scraping off any excess char (fread) */
+
+//////////////////////////////////////////////////////////////////////////
+/// Grills the given steak with a certain amount of BTUs, scraping off any excess char.
+/// Roughly comparable to fread.
+/// returns: Integer value representing how much grilling was done.
+//////////////////////////////////////////////////////////////////////////
 size_t grill(char* steak_char, steak steak_pointer, size_t btu)
 {
 	return fread(steak_char, btu, 1, steak_pointer.f);
 }
 
-/* Add an array of seasonings to the steak (fwrite) */
+
+//////////////////////////////////////////////////////////////////////////
+/// Adds an array of seasonings to the steak.
+/// Roughly comparable to fwrite.
+/// returns: An integer representation of how much seasoning was applied
+//////////////////////////////////////////////////////////////////////////
 size_t season(char* seasonings, size_t pinches, size_t num_seasonings, steak steak_pointer)
 {
 	return fwrite(seasonings, pinches, num_seasonings, steak_pointer.f);
 }
 
-/* Serve the current steak and place a new one on the same place of the grill */
+//////////////////////////////////////////////////////////////////////////
+/// Serves the current steak and places a new one on the same place of the grill.
+/// returns: The new steak object.
+//////////////////////////////////////////////////////////////////////////
 steak swap_steak(const char* steak_name, const char* prep_instructions, steak steak_pointer)
 {
 	char* mode = convert_mode(prep_instructions);
@@ -73,68 +117,120 @@ steak swap_steak(const char* steak_name, const char* prep_instructions, steak st
 	return s;
 }
 
-/* Remove the steak from the grill and serve it (fclose) */
+
+//////////////////////////////////////////////////////////////////////////
+/// Removes the steak from the grill and serves it.
+/// Roughly comparable to fclose.
+/// returns: Error code from removing the steak. Codes are the same as fclose.
+//////////////////////////////////////////////////////////////////////////
 int serve(steak steak_pointer)
 {
 	return fclose(steak_pointer.f);
 }
 
-/* (ftell) */
+
+//////////////////////////////////////////////////////////////////////////
+/// Checks exactly how red the steak is.
+/// Roughly comparable to ftell.
+/// returns: An integer representation of how well cooked the steak is.
+//////////////////////////////////////////////////////////////////////////
 long int check_redness(steak steak_pointer)
 {
 	return ftell(steak_pointer.f);
 }
 
-/* Perform an arcane ritual to undo any grilling to the steak (rewind) */
+
+//////////////////////////////////////////////////////////////////////////
+/// Performs an arcane ritual to undo any grilling done to the steak.
+/// Roughly comparable to the standard rewind function.
+//////////////////////////////////////////////////////////////////////////
 void ungrill(steak steak_pointer)
 {
-	return rewind(steak_pointer.f);
+	rewind(steak_pointer.f);
 }
 
-/* Perform an arcane ritual to adjust how red the steak is relative to its current state (fseek) */
+
+//////////////////////////////////////////////////////////////////////////
+/// Performs an arcane ritual to adjust how red the steak is relative to its current state.
+/// Roughly comparable to fseek
+/// returns: An integer representation of how well cooked the steak is after the ritual.
+//////////////////////////////////////////////////////////////////////////
 int adjust_redness(steak steak_pointer, long int offset, int redness)
 {
 	return fseek(steak_pointer.f, offset, redness);
 }
 
-/* (fputc) */
+
+//////////////////////////////////////////////////////////////////////////
+/// Adds a small dash of seasoning.
+/// Roughly equivalent to fputc
+/// returns: A clone of the added seasoning pulled from a parallel universe
+//////////////////////////////////////////////////////////////////////////
 int pinch_of_seasoning(int seasoning, steak steak_pointer)
 {
 	return fputc(seasoning, steak_pointer.f);
 }
 
 
-/* (fputs) */
+//////////////////////////////////////////////////////////////////////////
+/// Adds dashes of seasoning until the steak is well coated.
+/// Roughly comparable to fputs
+/// returns: A non-zero value or error code similar to fputs.
+//////////////////////////////////////////////////////////////////////////
 int dash_of_seasoning(const char* seasonings, steak steak_pointer)
 {
 	return fputs(seasonings, steak_pointer.f);
 }
 
-/* (fgetc) */
+
+//////////////////////////////////////////////////////////////////////////
+/// The flame gently kisses the steak, cooking it ever so slightly.
+/// Roughly equivalent to fgetc
+/// returns: The small bits of char created from the cooking.
+//////////////////////////////////////////////////////////////////////////
 int flame_kiss(steak steak_pointer)
 {
 	return fgetc(steak_pointer.f);
 }
 
-/* (fgets) */
+
+//////////////////////////////////////////////////////////////////////////
+/// Sears the steak on an open flame until it's cooked to perfection.
+/// Roughly equivalent to fgets
+/// returns: The char created from the cooking.
+//////////////////////////////////////////////////////////////////////////
 char* flame_sear(char* steak_char, steak steak_pointer, int btu)
 {
 	return fgets(steak_char, btu, steak_pointer.f);
 }
 
-/* (feof) */
-int is_overcooked(steak steak_pointer)
+
+//////////////////////////////////////////////////////////////////////////
+/// Checks whether the steak is done cooking.
+/// Roughly equivalent to feof.
+/// returns: Non-zero if the steak is well done, zero otherwise
+//////////////////////////////////////////////////////////////////////////
+int is_cooked(steak steak_pointer)
 {
 	return feof(steak_pointer.f);
 }
 
-/* (ferror) */ 
+
+//////////////////////////////////////////////////////////////////////////
+/// Checks whether the steak was left out a little took long and attracted a few maggots.
+/// Roughly equivalent to ferror.
+/// returns: The error code for the steak. Similar to ferror.
+//////////////////////////////////////////////////////////////////////////
 int is_rotten(steak steak_pointer)
 {
 	return ferror(steak_pointer.f);
 }
 
-/* (clearerr) */
+
+//////////////////////////////////////////////////////////////////////////
+/// Wipes off the maggots and green bits and gets cookin' anyway.
+/// Roughly equivalent to clearerr.
+//////////////////////////////////////////////////////////////////////////
 void remove_rotten_bits(steak steak_pointer)
 {
 	return clearerr(steak_pointer.f);
--- fsteak/fsteak.h
+++ fsteak/fsteak.h
@@ -1,51 +1,70 @@
+/*
+ * fsteak: A library for steak-based file I/O
+ * fsteak.h: Declarations of the fsteak structures and library functions 
+ * Copyright (C) 2014  Mike Lekon
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef F_STEAK_H
 #define F_STEAK_H
 
 struct steak;
 	
-/* Place a new steak on the grill (fopen) */
+/* Places a new steak on the grill (fopen) */
 steak fsteak(const char* steak_name, int steak_prep);
 
-/* Grill the given steak with a certain amount of BTUs, scraping off any excess char (fread) */
+/* Grills the given steak with a certain amount of BTUs, scraping off any excess char (fread) */
 size_t grill(char* steak_char, steak steak_pointer, int btu);
 
-/* Add an array of seasonings to the steak (fwrite) */
+/* Adds an array of seasonings to the steak (fwrite) */
 size_t season(char* seasonings, size_t pinches, size_t num_seasonings, steak steak_pointer);
 
-/* Serve the current steak and place a new one on the same place of the grill */
+/* Serves the current steak and places a new one on the same place of the grill */
 steak swap_steak(const char* steak_name, int steak_prep, steak steak_pointer);
 
-/* Remove the steak from the grill and serve it (fclose) */
+/* Removes the steak from the grill and serves it (fclose) */
 int serve(steak steak_pointer);
 
-/* (ftell) */
+/* Checks exactly how red the steak is (ftell) */
 long int check_redness(steak steak_pointer);
 
-/* Perform an arcane ritual to undo any grilling to the steak (rewind) */
+/* Performs an arcane ritual to undo any grilling done to the steak (rewind) */
 void ungrill(steak steak_pointer);
 
-/* Perform an arcane ritual to adjust how red the steak is relative to its current state (fseek) */
+/* Performs an arcane ritual to adjust how red the steak is relative to its current state (fseek) */
 int adjust_redness(steak steak_pointer, long int offset, int redness);
 
-/* (fputc) */
+/* Adds a small dash of seasoning (fputc) */
 int pinch_of_seasoning(int seasoning, steak steak_pointer);
 
-/* (fputs) */
+/* Adds dashes of seasoning until the steak is well coated (fputs) */
 int dash_of_seasoning(const char* seasonings, steak steak_pointer);
 
-/* (fgetc) */
+/* The flame gently kisses the steak, cooking it ever so slightly (fgetc) */
 int flame_kiss(steak steak_pointer);
 
-/* (fgets) */
+/* Sears the steak on an open flame until it's cooked to perfection (fgets) */
 char* flame_sear(char* steak_char, steak steak_pointer, int btu);
 
-/* (feof) */
-int is_overcooked(steak steak_pointer);
+/* Checks whether the steak is done cooking (feof) */
+int is_cooked(steak steak_pointer);
 
-/* (ferror) */ 
+/* Checks whether the steak was left out a little took long and attracted a few maggots (ferror) */ 
 int is_rotten(steak steak_pointer);
 
-/* (clearerr) */
+/* Wipes off the maggots and green bits and gets cookin' anyway (clearerr) */
 void remove_rotten_bits(steak steak_pointer);
 
 #endif
